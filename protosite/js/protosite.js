59aac106459c75f2e0508ceed024d31e202eb14a
jkngan <jasonkngan@gmail.com>
2015-12-12 15:11:23 -0800

    Image buttons working
    Clicking on the buttons moves to the correct page.

--- protosite/js/protosite.js
+++ protosite/js/protosite.js
@@ -1,6 +1,13 @@
-$(document).ready(function()) {
-	alert("bye");
-	$(".image_button").click(function()) {
-		alert("hi");
+$(document).ready(function() {
+	alert("javascript working");
+	$(".image_button").click(function() {
+		var paragraphs = this.getElementsByTagName("P");
+		if (paragraphs[0].textContent == "Lessons") {
+			window.location = "lessons.html";
+		} else if (paragraphs[0].textContent == "Assignments") {
+			window.location = "assignments.html";
+		} else if (paragraphs[0].textContent == "Quizzes") {
+			window.location = "quizzes.html";
+		}
 	});
-}
+});
