78e196810d8ea7b2c8a446b9fc8cf2a118a81b1a
jkngan <jasonkngan@gmail.com>
2015-12-13 02:42:37 -0800

    Added sample assignments

--- protosite/js/protosite.js
+++ protosite/js/protosite.js
@@ -9,11 +9,4 @@ $(document).ready(function() {
 			window.location = "quizzes.html";
 		}
 	});
-    
-    $("nav > ul > li").hover(function() {
-        var nav = this.getElementsByTagName("A")[0].textContent);
-        if (nav == "Lectures") {
-            $("#lectures_nav").show();
-        }
-    });
 });
